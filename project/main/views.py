5888f655206a19448dfc30c3b33a1a8f34217f8d
Николай <104462225+varvarian1@users.noreply.github.com>
2023-11-28 20:03:15 +0300

    Update views.py

--- project/main/views.py
+++ project/main/views.py
@@ -1,26 +1,21 @@
 from django.shortcuts import render, redirect
 from .forms import RegerForm
 
-# Create your views here.
-
 def index(request):
-    return render(request, 'main/index.html')
-
-def create(request):
-    error = ''
-    if request.method == 'POST':
-        form = ReferenceError(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-        else:
-            error = 'Форма неверна'
-
     form = RegerForm()
+    error = ''
 
     data = {
         'form': form,
         'error': error
     }
+
+    if request.method == 'POST':
+        form = RegerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма неверна'
 
     return render(request, 'main/index.html', data)
